a5c83d5f046114fc694e206e6a680337d90c8a74
gelvetica <helveticahelvetica@protonmail.com>
2024-12-13 14:58:14 -0500

    multiplatform dectalk ?

--- src/engines/dectalk/dectalk.py
+++ src/engines/dectalk/dectalk.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 ConfigStructure = {
     "advanced": {
@@ -15,18 +16,30 @@ class SapphoneEngine:
     def __init__(self, config):
         self.config = config
 
-    def build_command_args(self):
+    def dectalk_windows(self, script, output):
         args = []
         args += [self.config["engine"]["path_to_executable"]]
         if self.config["advanced"]["prefix"] != "":
             args += ["-pre", self.config["advanced"]["prefix"]]
         if self.config["advanced"]["suffix"] != "":
             args += ["-post", self.config["advanced"]["suffix"]]
-        return args
+        args += ["-w", output]
+        subprocess.run(args, shell=False, check=True)
+    def dectalk_linux(self, script, output):
+        args = []
+        args += [self.config["engine"]["path_to_executable"]]
+        if self.config["advanced"]["prefix"] != "":
+            args += ["-pre", self.config["advanced"]["prefix"]]
+        if self.config["advanced"]["suffix"] != "":
+            args += ["-post", self.config["advanced"]["suffix"]]
+        args += ["-fo", output]
+        args += ["-a", script]
+        subprocess.run(args, shell=False, check=True)
 
     def speak_to_file(self, script, output):
-        command = self.build_command_args()
-        command += ["-fo", output]
-        command += ["-a", script]
+        if sys.platform in ["win32", "cygwin"]:
+            return self.dectalk_windows(script, output)
+        elif sys.platform in ["linux", "darwin"]:
+            return self.dectalk_linux(script, output)
 
-        subprocess.run(command, shell=False, check=True)
+
